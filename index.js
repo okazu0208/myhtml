21a06ebb7a8538038cf159eef06660ab67229997
okazu <kazuya_y_y_y@outlook.jp>
2023-07-27 07:17:22 +0900

    rerere

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 var ws = new WebSocket('wss://cloud.achex.ca/comment');
-ws.onopen = function(evt){
- ws.send('{"auth":"saber@1471","passwd":"Koneko2514"}'); 
+
+ws.onopen = function(evt) {
+  ws.send('{"auth":"saber@1471","passwd":"Koneko2514"}');
 };
-ws.onmessage = function(evt){
- var commentdata = JSON.parse(evt.data);
- if (commentdata.auth == 'OK') {
-  return //OK jsonが来るのでここで関数を抜けます
- };
- console.log(commentdata)
- document.getElementById('comment').innerHTML = commentdata;
+
+ws.onmessage = function(evt) {
+  var commentdata = JSON.parse(evt.data);
+  if (commentdata.auth === 'OK') {
+    return; // OK jsonが来るのでここで関数を抜けます
+  }
+  console.log(commentdata);
+  document.getElementById('comment').innerHTML = commentdata.msg;
 };
-function click(){
- txt = document.getElementById(box).value;
- ws.onopen = function(evt){
- ws.send('{"to":"saber@1471", "msg":"test"}'); 
- console.log("iiiiiiiii")
- };
+
+function click() {
+  var txt = document.getElementById('box').value;
+  ws.send('{"to":"saber@1471", "msg":"' + txt + '"}');
+  console.log("コメント送信：" + txt);
 };
