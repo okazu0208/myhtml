a580731757190d5fc01486030ed9e2775703a652
okazu <kazuya_y_y_y@outlook.jp>
2023-07-26 19:58:12 +0900

    trtr

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-// index.js
-function linkWebSocket(){
- var ws = new WebSocket('wss://cloud.achex.ca/comment');
- ws.send((json.dumps({"auth":"saber@1471","passwd":"Koneko2514"})));
- ws.onmessage = function(evt){
-    var str = evt.data;
- };
-}
+var ws = new WebSocket('wss://cloud.achex.ca/comment');
+ws.send((json.dumps({"auth":"saber@1471","passwd":"Koneko2514"}))); 
+ws.onmessage = function(evt){
+ var str = evt.data;
+};
 document.getElementById('comment').innerHTML = str;
