a0c7ab2a07bf663fa957a4db722d9ce5676d700c
okazu <kazuya_y_y_y@outlook.jp>
2023-07-27 06:12:39 +0900

    えれれｒ

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var ws = new WebSocket('wss://cloud.achex.ca/comment');
 ws.onopen = function(evt){
- ws.send((JSON.stringify({"auth":"saber@1471","passwd":"Koneko2514"}))); 
+ ws.send('{"auth":"saber@1471","passwd":"Koneko2514"}'); 
 };
 ws.onmessage = function(evt){
- var str = JSON.parse(evt.data);
- if (str.auth == 'OK') {
-  return
+ var commentdata = JSON.parse(evt.data);
+ if (commentdata.auth == 'OK') {
+  return //OK jsonが来るのでここで関数を抜けます
  };
- document.getElementById('comment').innerHTML = str;
+ document.getElementById('comment').innerHTML = commentdata;
 };
