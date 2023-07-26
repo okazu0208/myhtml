0fdfcfd11cb0e0929d02f5792c17d2aed675af3d
okazu <kazuya_y_y_y@outlook.jp>
2023-07-26 20:05:53 +0900

    dsdsd

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var ws = new WebSocket('wss://cloud.achex.ca/comment');
-ws.send((json.dumps({"auth":"saber@1471","passwd":"Koneko2514"}))); 
+ws.send((JSON.stringify({"auth":"saber@1471","passwd":"Koneko2514"}))); 
 ws.onmessage = function(evt){
  var str = evt.data;
 };
