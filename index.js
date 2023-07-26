047b008d1f537124e8f45311ed59527e6c1678c0
okazu <kazuya_y_y_y@outlook.jp>
2023-07-27 05:49:35 +0900

    ewewew

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var ws = new WebSocket('wss://cloud.achex.ca/comment');
-ws.send((JSON.stringify({"auth":"saber@1471","passwd":"Koneko2514"}))); 
+ws.onopen = function(evt){
+ ws.send((JSON.stringify({"auth":"saber@1471","passwd":"Koneko2514"}))); 
+};
 ws.onmessage = function(evt){
  var str = evt.data;
 };
