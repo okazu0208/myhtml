779fc09592b253e5537fdf0fb13164fddd375df4
okazu <kazuya_y_y_y@outlook.jp>
2023-07-26 19:56:45 +0900

    gnhgh

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // index.js
 function linkWebSocket(){
  var ws = new WebSocket('wss://cloud.achex.ca/comment');
- ws.send("{"auth":"saber@1471","passwd":"Koneko2514"}");
+ ws.send((json.dumps({"auth":"saber@1471","passwd":"Koneko2514"})));
  ws.onmessage = function(evt){
     var str = evt.data;
  };
