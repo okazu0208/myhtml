cef0b770cb5237093695fc5e3893b8f83eaeddb1
okazu <kazuya_y_y_y@outlook.jp>
2023-07-27 18:15:15 +0900

    rere

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-var ws = new WebSocket('wss://cloud.achex.ca/comment');
-
-ws.onopen = function(evt) {
-  ws.send('{"auth":"saber@1471","passwd":"Koneko2514"}');
-};
-
-ws.onmessage = function(evt) {
-  var commentdata = JSON.parse(evt.data);
-  if (commentdata.auth === 'OK') {
-    return; // OK jsonが来るのでここで関数を抜けます
-  }
-  console.log(commentdata);
-  document.getElementById('comment').innerHTML = commentdata ;
-};
-
 function click() {
-  var txt = document.getElementById('box').value;
-  ws.onopen = function(evt) {
-   ws.send('{"to":"saber@1471","msg":"text"}');
-  };
-   console.log("i");
+  axios({
+    method: 'post',
+    url: 'https://api.dropboxapi.com/2/files/create_folder_v2',
+    data: JSON.stringify({
+      "path": "作成するフォルダのパス",
+      "autorename": false}), //autorenameについて：下記参照
+    headers: {
+      'Authorization': 'Bearer メモしたアクセストークン',
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => {
+    //接続に成功した場合の処理
+  })
+  .catch(error => {
+    //接続に失敗した場合の処理
+  })
+  .then(() => {
+    // 成功or失敗にかかわらず最後に実行するコード
+  })
 };
