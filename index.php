dd4906a45e2541af249ff9adfe5ee385c11967bd
okazu <kazuya_y_y_y@outlook.jp>
2023-08-16 19:45:57 +0900

    えれえ

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
 if (isset($_POST['posh'])) {
- $txt = $_GET["posh"];
-};
+ $txt = $_GET["text"];
+ require __DIR__ . '/vendor/autoload.php';
+ $options = array(
+  'cluster' => 'ap3',
+  'useTLS' => true
+ );
+ $pusher = new Pusher\Pusher(
+  '5c79a538be329c027f05',
+  '0936b6ce791c9beb060f',
+  '1651847',
+  $options
+ );
+ $data['message'] = $txt;
+ $pusher->trigger('my-channel', 'my-event', $data);
+ };
+?>
