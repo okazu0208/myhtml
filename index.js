292d4961b83d8f625e76f73a1d7f92cfb9fa4239
okazu <kazuya_y_y_y@outlook.jp>
2023-08-16 21:11:45 +0900

    yryry

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ channel.bind('my-event', function(data) {
  let text = document.getElementById('test').innerHTML;
  document.getElementById('test').innerHTML = JSON.stringify(data);
 });
+var Pusher = require('pusher');
+var pusher = new Pusher({
+  appId: '1651847',
+  key: '5c79a538be329c027f05',
+  secret: '0936b6ce791c9beb060f',
+  cluster: 'ap3',
+  useTLS: true,
+});
+pusher.trigger('my-channel', 'my-event', {
+  message: 'hello world'
+});
