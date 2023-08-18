ab9023c54e52ab69d6fb4be8ba2476e23984f48b
okazu <kazuya_y_y_y@outlook.jp>
2023-08-18 15:11:24 +0900

    ththt

--- index.js
+++ index.js
@@ -8,33 +8,12 @@ channel.bind('my-event', function(data) {
  document.getElementById('test').innerHTML = JSON.stringify(data);
 });
 function click() {
-    var xhr = new XMLHttpRequest();
-
-    // ハンドラの登録.
-    // onreadystatechange はどんなブラウザでも実装されている.
-    xhr.onreadystatechange = function() {
-        switch ( xhr.readyState ) {
-            case 0:
-                // 未初期化状態.
-                console.log( 'uninitialized!' );
-                break;
-            case 1: // データ送信中.
-                console.log( 'loading...' );
-                break;
-            case 2: // 応答待ち.
-                console.log( 'loaded.' );
-                break;
-            case 3: // データ受信中.
-                console.log( 'interactive... '+xhr.responseText.length+' bytes.' );
-                break;
-            case 4: // データ受信完了.
-                if( xhr.status == 200 || xhr.status == 304 ) {
-                    var data = xhr.responseText; // responseXML もあり
-                    console.log( 'COMPLETE! :'+data );
-                } else {
-                    console.log( 'Failed. HttpStatus: '+xhr.statusText );
-                }
-                break;
-        }
-    };    
+    cosole.log("OK")
+    xhr.open( 'POST', 'http://{送信先URL}/post.php', false );
+    // POST 送信の場合は Content-Type は固定.
+    xhr.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
+    // 
+    xhr.send( 'hoge=piyo&moge=fuga' );
+    xhr.abort(); // 再利用する際にも abort() しないと再利用できないらしい.
+    
 }
