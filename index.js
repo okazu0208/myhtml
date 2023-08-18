77965118be54a27744ccc9fa65ce7bc7b0f8d2a5
okazu <kazuya_y_y_y@outlook.jp>
2023-08-18 15:09:07 +0900

    edededede

--- index.js
+++ index.js
@@ -8,7 +8,33 @@ channel.bind('my-event', function(data) {
  document.getElementById('test').innerHTML = JSON.stringify(data);
 });
 function click() {
- var xhr = new XMLHttpRequest();
- xhr.open("GET", "https://raw.githubusercontent.com/okazu0208/myhtml/main/index.py?value=" + encodeURIComponent(txt.value), true);
- xhr.send();
+    var xhr = new XMLHttpRequest();
+
+    // ハンドラの登録.
+    // onreadystatechange はどんなブラウザでも実装されている.
+    xhr.onreadystatechange = function() {
+        switch ( xhr.readyState ) {
+            case 0:
+                // 未初期化状態.
+                console.log( 'uninitialized!' );
+                break;
+            case 1: // データ送信中.
+                console.log( 'loading...' );
+                break;
+            case 2: // 応答待ち.
+                console.log( 'loaded.' );
+                break;
+            case 3: // データ受信中.
+                console.log( 'interactive... '+xhr.responseText.length+' bytes.' );
+                break;
+            case 4: // データ受信完了.
+                if( xhr.status == 200 || xhr.status == 304 ) {
+                    var data = xhr.responseText; // responseXML もあり
+                    console.log( 'COMPLETE! :'+data );
+                } else {
+                    console.log( 'Failed. HttpStatus: '+xhr.statusText );
+                }
+                break;
+        }
+    };    
 }
