3e13cb26960567f84aa0b893242b2594b41655a6
okazu <kazuya_y_y_y@outlook.jp>
2023-08-19 15:31:36 +0900

    rerer

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var ws = new WebSocket('wss://cloud.achex.ca/sand');
+var n = document.cookie;
 ws.onopen = function(evt){
  ws.send('{"auth":"server@1471", "password":"Koneko2514"}');
 };
 ws.onmessage = function(evt){
  var msg = JSON.parse(evt.data);
  let text = document.getElementById('test').innerHTML;
- document.getElementById('test').innerHTML = JSON.stringify(msg.msg);
- function push(){
-    Push.create('うんこ');
- }
+ console.log(msg)
+ document.getElementById('test').innerHTML = msg.sID + '=' + msg.msg;
+ document.cookie = msg.sID + '=' + msg.msg;
 };
 let elem = document.getElementById('bt');
 elem.onclick = function() {
  ws.send(JSON.stringify({"to":"server@1471", "msg": txt.value}));
-}
+};
