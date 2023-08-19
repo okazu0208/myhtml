8dd66485f73946b4173f6f39f50b45b8575bac0a
okazu <kazuya_y_y_y@outlook.jp>
2023-08-19 09:29:59 +0900

    ewewew

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-Pusher.logToConsole = true;
-var pusher = new Pusher('5c79a538be329c027f05', {
- cluster: 'ap3'
-});
-var channel = pusher.subscribe('my-channel');
-channel.bind('my-event', function(data) {
+var ws = new WebSocket('wss://cloud.achex.ca/sand');
+ws.onopen = function(evt){
+ ws.send('{"auth":"server@1471", "password":"Koneko2514"}');
+ ws.send('{"to":"server@1471", "msg":"test"}');
+};
+ws.onmessage = function(evt){
+ var msg = evt.data;
  let text = document.getElementById('test').innerHTML;
- document.getElementById('test').innerHTML = JSON.stringify(data);
-});
+ document.getElementById('test').innerHTML = JSON.stringify(msg);
+};
 let elem = document.getElementById('bt');
 elem.onclick = function() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://raw.githubusercontent.com/okazu0208/myhtml/main/index.php", true);
-    xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-        var response = xhr.responseText;
-        // PHPスクリプトの実行結果を処理する
-        console.log(response);
-    }
-    };
-    xhr.send();
 }
