f6ff6742152e5956e7d668809f43382d37467aa4
okazu <kazuya_y_y_y@outlook.jp>
2023-08-20 11:37:11 +0900

    555

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var ws = new WebSocket('wss://cloud.achex.ca/sand');
-var n = document.cookie.split(';').length
+var n = document.cookie.split(';').length//くっきーの長さ取得
 console.log(n)
-if (n !== 1){
+if (n !== 1){//データが残っていたらその列を表示
  for (var i = 0; i<n;  ++i) {
   var text = document.getElementById('test').innerHTML;
   document.getElementById('test').innerHTML = text + '</br>' + decodeURI(document.cookie.split(';')[i]);
  }
  n++;
 }
-ws.onopen = function(evt){
+ws.onopen = function(evt){//websoclet通信
  ws.send('{"auth":"server@1471", "password":"Koneko2514"}');
 };
-ws.onmessage = function(evt){
+ws.onmessage = function(evt){//イベント
  var msg = JSON.parse(evt.data);
  if (msg.auth !== "OK"){
  n++;
@@ -22,6 +22,7 @@ ws.onmessage = function(evt){
  }
 };
 let elem = document.getElementById('bt');
-elem.onclick = function() {
+elem.onclick = function() {//送信
  ws.send(JSON.stringify({"to":"server@1471", "msg": txt.value}));
+ txt.value = '';
 };
