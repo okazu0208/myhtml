adb6400786ce944355467a721a128b6ff11d025f
okazu <kazuya_y_y_y@outlook.jp>
2023-08-20 10:38:55 +0900

    ererer

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 var ws = new WebSocket('wss://cloud.achex.ca/sand');
-var n = document.cookie;
+var n = document.cookie.split(';').length
+console.log(n)
+if (n !== 1){
+ for (var i = 0; i<n;  ++i) {
+  var text = document.getElementById('test').innerHTML;
+  document.getElementById('test').innerHTML = text + '</br>' + document.cookie.split(';')[i];
+ }
+ n++;
+}
 ws.onopen = function(evt){
  ws.send('{"auth":"server@1471", "password":"Koneko2514"}');
 };
 ws.onmessage = function(evt){
  var msg = JSON.parse(evt.data);
- let text = document.getElementById('test').innerHTML;
- console.log(msg)
- document.getElementById('test').innerHTML = msg.sID + '=' + msg.msg;
- document.cookie = msg.sID + '=' + msg.msg;
- console.log(document.cookie.split('='))
+ if (msg.auth !== "OK"){
+ n++;
+ var text = document.getElementById('test').innerHTML;
+ document.getElementById('test').innerHTML = text + '</br>' + n + '=' + msg.sID + '=' + msg.msg;
+ document.cookie = n + '=' +  msg.sID + '=' + msg.msg;
+ console.log(n);
+ }
 };
 let elem = document.getElementById('bt');
 elem.onclick = function() {
